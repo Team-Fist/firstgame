5b2e8fe248bbedeb1b35feadb9f9394b2ea76890
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2020-11-12 13:36:13 -0800

    commit

---
+++ Assets/Scripts/MoveAndCam/Facing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Facing : MonoBehaviour
+{
+    public GameObject Self;
+    public GameObject Other;
+    public bool facing = true;
+
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target
+        if (facing)
+        {
+            Self.transform.LookAt(Other.transform);
+        }
+        else
+        {
+            Self.transform.LookAt(Self.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public AttackController attackController;
+    public MoveController moveController;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            attackController.straightPunch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            moveController.canMove = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            attackController.midKick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            attackController.startBlock();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            attackController.endBlock();
+        }
+    }
+}
