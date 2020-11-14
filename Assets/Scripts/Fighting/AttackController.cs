f35c802dade18630ace0ca0cd23457275de47809
SamHadavi <32000449+SamHadavi@users.noreply.github.com>
2020-11-13 16:12:24 -0800

    fix

--- Assets/Scripts/Fighting/AttackController.cs
+++ Assets/Scripts/Fighting/AttackController.cs
@@ -6,9 +6,11 @@ public class AttackController : MonoBehaviour
 {
     public Animator animator;
     public MoveController moveController;
+    public Facing facing;
     public bool canAttack = true;
     public bool isPunching = false;
     public bool isKicking = false;
+    public bool isBlocking = false;
     public bool punchStraight = false;
     public bool punchHook = false;
     public bool punchUpper = false;
@@ -16,71 +18,225 @@ public class AttackController : MonoBehaviour
     public bool kickLow = false;
     public bool kickHigh = false;
 
-    private void Start()
+    private void Start(){}
+
+    public void startBlock()
     {
-        moveController = GetComponent<MoveController>();
+        if (canAttack && !isPunching && !isKicking)
+        {
+            animator.SetBool("Blocking", true);
+            isBlocking = true;
+        }
+    }
+
+    public void endBlock()
+    {
+        if (canAttack && !isPunching && !isKicking)
+        {
+            animator.SetBool("Blocking", false);
+            isBlocking = false;
+        }
+    }
+
+    public void straightPunch()
+    {
+        if (canAttack)
+        {
+            if (!punchStraight)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchStraightLeft");
+            }
+            else if (punchStraight)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchStraightRight");
+            }
+        }
+    }
+
+    public void hookPunch()
+    {
+        if (canAttack)
+        {
+            if (!punchHook)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchHookLeft");
+            }
+            else if (punchHook)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchHookRight");
+            }
+        }
+    }
+
+    public void upperPunch()
+    {
+        if (canAttack)
+        {
+            if (!punchUpper)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchUpperLeft");
+            }
+            else if (punchUpper)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("PunchUpperRight");
+            }
+        }
+    }
+
+    public void midKick()
+    {
+        if (canAttack)
+        {
+            if (!kickMid)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("KickMidLeft");
+            }
+            else if (kickMid)
+            {
+                moveController.isMoving = false;
+                animator.SetTrigger("KickMidRight");
+            }
+        }
     }
 
     void Update()
     {
-        // punch straight
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Block_Hold"))
         {
             moveController.canMove = false;
-            if (canAttack)
-            {
-                if (!punchStraight)
-                {
-                    moveController.isMoving = false;
-                    animator.SetTrigger("PunchStraightLeft"); 
-                }
-                else if (punchStraight)
-                {
-                    moveController.isMoving = false;
-                    animator.SetTrigger("PunchStraightRight");
-                }
-            }
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Block_Release"))
+        {
+            moveController.canMove = false;
+        }
+        else
+        {
+            moveController.canMove = true;
         }
 
-        /*/ punch hook
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKeyDown(KeyCode.W))
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Straight_Left"))
         {
-            if (canAttack)
-            {
-                if (!punchStraight)
-                {
-                    moveController.isMoving = false;
-                    moveController.canMove = false;
-                    punchStraight = true;
-                    animator.SetTrigger("PunchHookLeft");
-                }
-                else if (punchStraight)
-                {
-                    moveController.isMoving = false;
-                    moveController.canMove = false;
-                    animator.SetTrigger("PunchHookRight");
-                }
-            }
-        }*/
-
-
-        // kick mid 
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = true;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
             moveController.canMove = false;
-            if (canAttack)
-            {
-                if (!kickMid)
-                {
-                    moveController.isMoving = false;
-                    animator.SetTrigger("KickMidLeft");
-                }
-                else if (kickMid)
-                {
-                    moveController.isMoving = false;
-                    animator.SetTrigger("KickMidRight");
-                }
-            }
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Straight_Right"))
+        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Hook_Left"))
+        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = true;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Hook_Right"))
+        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Upper_Left"))
+        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = true;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punch_Upper_Right"))
+        {
+            isPunching = true;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_Mid_Left"))
+        {
+            isPunching = false;
+            isKicking = true;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = true;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_Mid_Right"))
+        {
+            isPunching = false;
+            isKicking = true;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = false;
+            moveController.canMove = false;
+        }
+        else
+        {
+            isPunching = false;
+            isKicking = false;
+            punchStraight = false;
+            punchHook = false;
+            punchUpper = false;
+            kickMid = false;
+            kickLow = false;
+            kickHigh = false;
+            facing.facing = true;
+            moveController.canMove = true;
         }
     }
 }
--- Assets/Scripts/MoveAndCam/MoveController.cs
+++ Assets/Scripts/MoveAndCam/MoveController.cs
@@ -5,118 +5,44 @@ using UnityEngine;
 public class MoveController : MonoBehaviour
 {
     public Animator animator;
-    
-    private Rigidbody rb;
+    public AttackController attackController;
+    public Facing facing;
+    public Rigidbody rb;
+    public CharacterController characterController;
+    public bool canMove = true;
+    public bool isMoving = false;
+    public bool canJump = true;
+    public bool isJumping = false;
+
+
     public float thrust = 5.0f;
 
-    public float speed = 1.0f;
+    public float speed = 2.0f;
+
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
 
-    CharacterController characterController;
-    public AttackController attackController;
     Vector3 moveDirection = Vector3.zero;
     Vector2 rotation = Vector2.zero;
 
-    public bool canMove = true;
-    public bool isMoving = false;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        attackController = GetComponent<AttackController>();
-        rotation.y = transform.eulerAngles.y;
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (attackController.isKicking == true || attackController.isPunching == true)
-        {
-            canMove = false;
-        }
-        else
-        {
-            canMove = true;
-        }
-
-
-        //If the player is moving horizontally (left and right), and not diagonally or vertically
-        //Remember: 1 is right (positive), and -1 is left (negative).
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveForward", true);
-                isMoving = true;
-            }
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveForward", true);
-                isMoving = true; 
-            }
-        }
-        else //if (Input.GetAxis("Horizontal") == 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveForward", false);
-                isMoving = false; 
-            }
-        }
-
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveForward", true);
-                isMoving = true; 
-            }
-        }
-        else if (Input.GetAxis("Vertical") < 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveBackward", true);
-                isMoving = true; 
-            }
-        }
-        else //if (Input.GetAxis("Vertical") == 0)
-        {
-            if (canMove)
-            {
-                animator.SetBool("MoveBackward", false);
-                isMoving = false; 
-            }
-        }
+       
     }
 
     void Update()
     {
         if (canMove)
         {
-            if (characterController.isGrounded)
-            {
-
-                // We are grounded, so recalculate move direction based on axes
-                Vector3 forward = transform.TransformDirection(Vector3.forward);
-                Vector3 right = transform.TransformDirection(Vector3.right);
-                float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
-                float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
-                moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-
-                if (Input.GetButton("Jump") && canMove)
-                {
-                    moveDirection.y = jumpSpeed;
-                }
-            } 
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            float curSpeedX = canMove ? speed * Input.GetAxisRaw("Vertical") : 0;
+            float curSpeedY = canMove ? speed * Input.GetAxisRaw("Horizontal") : 0;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            
         }
         else
         {
@@ -126,9 +52,78 @@ public class MoveController : MonoBehaviour
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
-
-        // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
-        //rb.AddForce(moveDirection * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            attackController.canAttack = false;
+        }
+        else if (!isMoving)
+        {
+            attackController.canAttack = true;
+        }
+
+        if (attackController.isKicking == true || attackController.isPunching == true)
+        {
+            canMove = false;
+        }
+        else
+        {
+            canMove = true;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveForward", true);
+                isMoving = true;
+            }
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveForward", true);
+                isMoving = true;
+            }
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveForward", false);
+                isMoving = false;
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveForward", true);
+                isMoving = true;
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveBackward", true);
+                isMoving = true;
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            if (canMove)
+            {
+                animator.SetBool("MoveBackward", false);
+                isMoving = false;
+            }
+        }
     }
 }
